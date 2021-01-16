e2b383ae7aa2d18378b51f71dd8e59bb9faea4b5
Joseph Daniel Winters <jdwinters96@gmail.com>
2021-01-16 17:35:09 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function numDaysInMonth(month: number, year: number): number {
+  // NB: Months are 0-indexed (i.e. January = 0). Day 0 of the following month is the last day of the given month.
+  return new Date(year, month + 1, 0).getDate();
+}
+
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,12 +16,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>

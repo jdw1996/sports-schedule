0900d1aff3bc0ae351367ceeffa62178bc3071cd
Joseph Daniel Winters <jdwinters96@gmail.com>
2021-01-31 15:30:00 -0500

    Add missing field in Game

--- src/espn-client.ts
+++ src/espn-client.ts
@@ -1,3 +1,4 @@
+import { Colour } from './colours';
 import { Game } from './games';
 
 const URLS: Map<string, string> = new Map([
@@ -12,7 +13,7 @@ type ESPNGame = {
   shortName: string;
 };
 
-function espnToNative(espn: ESPNGame, teamOfInterest: string): Game {
+function espnToNative(espn: ESPNGame, teamOfInterest: string, colour: Colour): Game {
   const [away, , home] = espn.shortName.split(' ');
   let opponentString = '';
   if (away === teamOfInterest) {
@@ -20,14 +21,14 @@ function espnToNative(espn: ESPNGame, teamOfInterest: string): Game {
   } else {
     opponentString = `v${away}`;
   }
-  return { opponentString, date: new Date(espn.date) };
+  return { opponentString, date: new Date(espn.date), colour };
 }
 
-export async function getSchedule(league: string, team: string): Promise<Game[]> {
+export async function getSchedule(league: string, team: string, colour: Colour): Promise<Game[]> {
   if (!URLS.has(league)) {
     throw new Error(`Given value ${league} is not a known league.`);
   }
   const url = `${URLS.get(league)}${team}/schedule`;
   const events: ESPNGame[] = (await (await fetch(url)).json()).events;
-  return events.map((espn) => espnToNative(espn, team));
+  return events.map((espn) => espnToNative(espn, team, colour));
 }
